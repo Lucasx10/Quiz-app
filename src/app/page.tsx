f3cb5b1a8c9d1c748390dda0ea4bcb9358248ab0
Lucasx10 <lukasprado260@gmail.com>
2023-09-15 12:35:18 -0400

    Adicionando Formulario e contexto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,65 @@
-import Link from "next/link";
+"use client";
+
+import { useGlobalContext } from "@/context/main";
+import { useRouter } from "next/navigation";
+import { FormEventHandler, useState } from "react";
 
 export default function Home() {
+  // const [newName, setNewName] = useState("");
+  // const [newEmail, setNewEmail] = useState("");
+
+  const { newName, setNewName, newEmail, setNewEmail } = useGlobalContext();
+
+  const router = useRouter();
+
+  const handleSubmitIniciar: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    //Ir para pagina quiz
+    router.push("/quiz");
+  };
+
   return (
     <main className="flex flex-col items-center gap-3">
-      <p className="text-lg text-center">Teste seus conhecimentos sobre ciência da computação !</p>
-      <Link href="/quiz">
-        <button className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-full">
-          Start Quiz
-        </button>
-      </Link>
+      <p className="text-lg text-center">
+        Teste seus conhecimentos sobre ciência da computação !
+      </p>
+
+      <div>
+        <form
+          onSubmit={handleSubmitIniciar}
+          className="flex flex-col items-center gap-2"
+        >
+          <div>
+            <label htmlFor="newName">Nome: </label>
+            <input
+              type="text"
+              name="newName"
+              id="newName"
+              className="text-black p-2"
+              value={newName}
+              onChange={(e) => setNewName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              name="newEmail"
+              id="newEmail"
+              className="text-black p-2"
+              value={newEmail}
+              onChange={(e) => setNewEmail(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-full"
+          >
+            Start Quiz
+          </button>
+        </form>
+      </div>
     </main>
   );
 }
